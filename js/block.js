c66c3d04d2097f1ac4e0f986546f761359499fb4
KrOySi <67242784+KrOySi@users.noreply.github.com>
2025-04-10 19:35:38 +0300

    fix

--- js/block.js
+++ js/block.js
@@ -1,4 +1,44 @@
 let url = '/videos/Righthere.mp4';
-		window.addEventListener('devtoolschange', event => {
-				window.location = url;
+
+(function devtoolsDetector() {
+    const threshold = 160;
+    let lastTime = performance.now();
+
+    function check() {
+        const start = performance.now();
+
+        setTimeout(() => {
+            requestAnimationFrame(() => {
+                const delta = performance.now() - start;
+                const delay = performance.now() - lastTime;
+
+                if (delta > threshold || delay > threshold * 2) {
+                    window.location = url;
+                }
+
+                lastTime = performance.now();
+                check();
+            });
+        }, 0);
+
+        try {
+            Function('debugger')();
+        } catch (e) {
+        }
+    }
+
+    check();
+
+    window.addEventListener('resize', () => {
+        if (window.innerWidth <= 500) {
+            window.location = url;
+        }
     });
+
+    window.addEventListener('devtoolschange', function(e) {
+        if (e.detail.open) {
+            window.location = url;
+        }
+    });
+
+})();
